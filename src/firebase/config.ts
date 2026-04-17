a0ee1fa66a8b8a997ac3bfb8e659e762ace09bfd
Edna Card <ednacard87@gmail.com>
2026-04-17 01:28:06 +0000

    se configuró el config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,8 @@
-// This file will be manually updated by the user.
-// You can find your project's configuration in the Firebase Console.
-
 export const firebaseConfig = {
-  projectId: "studio-1908798070-d8011",
-  appId: "1:327530640689:web:4047183ab8e6213e942c10",
   apiKey: "AIzaSyA5RA5fP2y9J_iMx1rbOPHYHNWpPijMm_w",
   authDomain: "studio-1908798070-d8011.firebaseapp.com",
-  measurementId: "",
+  projectId: "studio-1908798070-d8011",
+  storageBucket: "studio-1908798070-d8011.firebasestorage.app",
   messagingSenderId: "327530640689",
-  storageBucket: "studio-1908798070-d8011.appspot.com"
+  appId: "1:327530640689:web:aa696d78b7ae962a942c10"
 };
